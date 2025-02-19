a10145c4c47326f23c04731667ef3d090f3fd2ea
Art <artour.borisenko@ya.ru>
2025-02-19 17:10:58 +0300

    init commit

---
+++ src/client/java/com/mcmodding/fabricafkfarm/client/config/ModConfigScreen.java
@@ -0,0 +1,13 @@
+package com.mcmodding.fabricafkfarm.client.config;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class ModConfigScreen implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+    }
+}
---
+++ src/main/java/com/mcmodding/fabricafkfarm/FabricAFKFarmMod.java
@@ -0,0 +1,10 @@
+package com.mcmodding.fabricafkfarm;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricAFKFarmMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Client-side mod doesn't need server initialization
+    }
+}
